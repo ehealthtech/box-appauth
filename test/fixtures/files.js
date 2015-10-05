d9c360416fbf02ca68c63528227461523db81412
Sandro Pasquali <sandro.pasquali@ehealthtechnologies.com>
2015-10-05 00:21:57 -0400

    added shared link and api download test (byte comparison)

--- test/fixtures/files.js
+++ test/fixtures/files.js
@@ -1,47 +1,49 @@
 "use strict";
 
 var util = require('util');
+var request = require('request');
+
 var _ = require('lodash');
 
 module.exports = function(api, test, Promise) {
 
-	return api
-	.folder.create({
-		parentId: 0,
-		name: 'uploads_temp',
-		fields: [
-			'name',
-			'parent'
-		]
-	})
-	.bind({}) // Is #this through promise chain 
+    return api
+    .folder.create({
+        parentId: 0,
+        name: 'uploads_temp',
+        fields: [
+            'name',
+            'parent'
+        ]
+    })
+    .bind({}) // Is #this through promise chain
 
-	.then(function(res) {
-	
-		console.log('** Upload folder created ->\n', res);
+    .then(function(res) {
 
-		this.tempUploadId = res.id;
+        console.log('** Upload folder created ->\n', res);
 
-		return api.file.preflightCheck({
-			name: 'mynewfile.jpg',
-			parentId: res.id
-		})
-	})
-	.then(function(res) {
-	
-		console.log('** Preflight Status ->\n', res);
-		
-		var tmp = this.tempUploadId;
+        this.tempUploadId = res.id;
 
-		return api.file.upload({
-			name: 'mynewfile.jpg',
-			file: __dirname + '/assets/test_image_upload.jpg',
-			parentId: tmp,
-			fields: [
-				'total_count'
-			]
-		})
-	})
+        return api.file.preflightCheck({
+            name: 'mynewfile.jpg',
+            parentId: res.id
+        })
+    })
+    .then(function(res) {
+
+        console.log('** Preflight Status ->\n', res);
+
+        var tmp = this.tempUploadId;
+
+        return api.file.upload({
+            name: 'mynewfile.jpg',
+            file: __dirname + '/assets/test_image_upload.jpg',
+            parentId: tmp,
+            fields: [
+                'total_count'
+            ]
+        })
+    })
     .then(function(res) {
         console.log('**  Upload ->\n', util.inspect(res, {depth:10}));
 
@@ -57,79 +59,101 @@ module.exports = function(api, test, Promise) {
     .then(function(res) {
         console.log('**  Shared Link Created ->\n', util.inspect(res, {depth:10}));
 
+        return new Promise(function(resolve, reject) {
+            return request(res.shared_link.download_url, function(error, response, body) {
+                if(!error && response.statusCode == 200) {
+                    return resolve(body);
+                }
+                reject(error);
+            })
+        })
+    })
+    .then(function(res) {
+
+        var bytesdl = Buffer.byteLength(res, 'utf8');
+
+        console.log('**  Download via Shared Link ->', bytesdl, 'bytes');
+
+        this.sharedLinkBytes = bytesdl;
+
         var dlid = this.upFileId;
 
         return api.file.download({
             id: dlid
         })
     })
-	.then(function(res) {
-		console.log('**  Download ->', Buffer.byteLength(res, 'utf8'), 'bytes');
-		
-		var tmp = this.tempUploadId;
+    .then(function(res) {
 
-		return api.folder.list({
-			id: tmp
-		})
-	})
-	.then(function(res) {
-		console.log('** Upload folder listing after upload ->\n', res);
-		
-		var upId = this.upFileId;
-		
-		return api.file.info({
-			id: upId,
-			fields: [
-				'file_version',
-				'size',
-				'parent'
-			]
-		});
-	})
-	.then(function(res) {
-		console.log('** Uploaded file info ->\n', util.inspect(res, {depth: 10}));
-		
-		var upId = this.upFileId;
-		
-		return api.file.updateInfo({
-			id: upId,
-			name: 'booberry.jpg',
-			description: 'This describes a new booberry file',
-			tags: [
-				'some',
-				'tags',
-				'here'
-			],
-			fields : [
-				'name',
-				'file_version',
-				'tags',
-				'description'
-			]
-		});
-	})
-	.then(function(res) {
-		console.log('** Updated file info ->\n', res);
+        var bytesdl = Buffer.byteLength(res, 'utf8');
 
-		var tmp = this.tempUploadId;
-		
-		return api.folder.list({
-			id: tmp,
-			fields: []
-		})
-	})
-	.then(function(res) {
-		console.log('** Upload folder list after file info updated ->\n', res);
-		
-		var upId = this.upFileId;
+        console.log('**  Download via API) ->', bytesdl, 'bytes');
 
-		// Upload a new version of the file.
-		//
-		return api.file.update({
-			id: upId,
+        test.equal(bytesdl, this.sharedLinkBytes, 'Shared Link Download and API download returned equal # of bytes (' + bytesdl + ')');
+
+        var tmp = this.tempUploadId;
+
+        return api.folder.list({
+            id: tmp
+        })
+    })
+    .then(function(res) {
+        console.log('** Upload folder listing after upload ->\n', res);
+
+        var upId = this.upFileId;
+
+        return api.file.info({
+            id: upId,
+            fields: [
+                'file_version',
+                'size',
+                'parent'
+            ]
+        });
+    })
+    .then(function(res) {
+        console.log('** Uploaded file info ->\n', util.inspect(res, {depth: 10}));
+
+        var upId = this.upFileId;
+
+        return api.file.updateInfo({
+            id: upId,
+            name: 'booberry.jpg',
+            description: 'This describes a new booberry file',
+            tags: [
+                'some',
+                'tags',
+                'here'
+            ],
+            fields : [
+                'name',
+                'file_version',
+                'tags',
+                'description'
+            ]
+        });
+    })
+    .then(function(res) {
+        console.log('** Updated file info ->\n', res);
+
+        var tmp = this.tempUploadId;
+
+        return api.folder.list({
+            id: tmp,
+            fields: []
+        })
+    })
+    .then(function(res) {
+        console.log('** Upload folder list after file info updated ->\n', res);
+
+        var upId = this.upFileId;
+
+        // Upload a new version of the file.
+        //
+        return api.file.update({
+            id: upId,
             file: __dirname + '/assets/test_image_update.jpg',
-		});
-	})
+        });
+    })
     .then(function(res) {
         console.log('** File version updated ->\n', util.inspect(res, {depth: 10}));
 
@@ -159,35 +183,35 @@ module.exports = function(api, test, Promise) {
             ]
         });
     })
-	.then(function(res) {
-		console.log('** Uploaded file copied ->\n', util.inspect(res, {depth: 10}));
-		
-		var upId = this.upFileId;
-		
-		return api.file.delete({
-			id: upId
-		});
-	})
-	.then(function(res) {
-		console.log('** Deleted file info ->\n', res);
+    .then(function(res) {
+        console.log('** Uploaded file copied ->\n', util.inspect(res, {depth: 10}));
 
-		var tmp = this.tempUploadId;
-		
-		return api.folder.list({
-			id: tmp
-		})
-	})
-	.then(function(res) {
-		console.log('** Upload Folder listing after delete ->\n', util.inspect(res, {depth: 10}));
-	})
-	.finally(function() {
-		console.log('** Deleting upload folder ->\n');
-		
-		var tmp = this.tempUploadId;
-		
-		api.folder.delete({
-			id: tmp,
-			recursive: true
-		});
-	});
+        var upId = this.upFileId;
+
+        return api.file.delete({
+            id: upId
+        });
+    })
+    .then(function(res) {
+        console.log('** Deleted file info ->\n', res);
+
+        var tmp = this.tempUploadId;
+
+        return api.folder.list({
+            id: tmp
+        })
+    })
+    .then(function(res) {
+        console.log('** Upload Folder listing after delete ->\n', util.inspect(res, {depth: 10}));
+    })
+    .finally(function() {
+        console.log('** Deleting upload folder ->\n');
+
+        var tmp = this.tempUploadId;
+
+        api.folder.delete({
+            id: tmp,
+            recursive: true
+        });
+    });
 }; 
