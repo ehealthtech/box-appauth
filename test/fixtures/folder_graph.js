74102730f7102d3d18170c378ea69f9812191314
Sandro Pasquali <sandro.pasquali@ehealthtechnologies.com>
2015-10-07 18:00:25 -0400

    finished #move method; refactored #graph; version bump

--- test/fixtures/folder_graph.js
+++ test/fixtures/folder_graph.js
@@ -16,6 +16,20 @@ module.exports = function(api, test, Promise) {
         test.ok(graph.lookup, '#graph returned a lookup table');
 
         console.log(util.inspect(graph, {depth: 10}));
+/*
+        api.folder.move({
+            sourceId: 4900457265,
+            destinationId: 4900414029,
+            name: 'fuzzy floozie',
+            recursive: true
+        })
+        .then(function(boxR) {
+            console.log('MOVING-->', boxR)
+        })
+        .catch(function(err) {
+            console.log('moving error', err)
+        })
+*/
     })
     .catch(function(err) {
         console.log('folder.graph Error:', err);
